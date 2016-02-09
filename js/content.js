c7481691097449dd81261b83a000c09d152c94a8
tabish.khan <tabish.khan@tothenew.com>
2016-02-09 12:44:29 +0530

    test files added

---
+++ js/content.js
@@ -0,0 +1,336 @@
+var content = [
+    {
+        "id": "en-gb",
+        "code": "en-gb",
+        "directory": "uk",
+        "title": "UK English",
+        "aboutDK": {
+            "url": "http://www.dk.com/information/about-dk/",
+            "title": "About DK"
+        },
+        "DKImages": {
+            "url": "https://www.dkimages.com/",
+            "title": "DK Images"
+        },
+        dialog: [{
+            "name": "delete-lesson-plan",
+            "title": "Are you sure you want to delete this lesson plan?",
+            "description": "",
+            "buttons": [{"title": "No, I want to keep it &rsaquo;", "type": 1, "url": "javascript:;", "destination": true}, {"title":"Yes, delete forever","type": 2,"url": "javascript:;", "destination": true}]
+        }],
+        meta: [
+            {
+                "name": "not-found",
+                "title": "Page not found",
+                "description": ""
+            }, {
+                "name": "home",
+                "title": "DK Find Out! | Fun Facts for Kids on Animals, Earth, History and more!",
+                "description": "Visit DK Find Out! to learn about dinosaurs, space, animals, nature &amp; more. Fun learning activities for kids with videos, quizzes & galleries at DKFindOut.com"
+            }, {
+                "name": "discover",
+                "title": "Discover | DK Find Out!",
+                "description": "Discover all the subjects on DK Find Out"
+            }, {
+                "name": "lesson-plan",
+                "title": "Build my lesson plan | DK Find Out!",
+                "description": ""
+            }, {
+                "name": "explore",
+                "title": "Explore | DK Find Out!",
+                "description": ""
+            }
+        ],
+        staticMessaging: [
+            {
+                "name": "homeIntro",
+                "description": "This is the intro copy on the homepage",
+                "copy": "What do you want to find out?",
+            }, {
+                "name": "homeSubjects",
+                "description": "Header for subjects on HP",
+                "copy": "What&#8217;s in <span class='hide'>DKFindOut!</span><img src='/images/ie/logo.png' alt='' role='presentation' />",
+            }, {
+                "name": "homeStatics",
+                "description": "Header for parents/teachers on HP",
+                "copy": "There&#8217;s more to <span class='hide'>DKFindOut!</span><img src='/images/ie/logo.png' alt='' role='presentation' />",
+            }, {
+                "name": "copyright",
+                "description": "This is the footer legal information",
+                "copy": "<p>&copy; 2015 Dorling Kindersley Limited. Registered Number 01177822 England. Registered Office: 80 Strand, London, WC2R 0RL.</p><p>'Dorling Kindersley', 'DK', 'Eyewitness' and the open book logo <img src='http://res.cloudinary.com/dorling-kindersley/image/upload/DK/9b5cd3b1a3ee4d48b3b1cbf37f5d51d3/0edf65b931034cde91ccca3f89610565.jpg' alt='DK Books' style='vertical-align: middle;'> are trade marks of Dorling Kindersley Limited.</p>",
+            }, {
+                "name": "privateBrowsing",
+                "description": "Message if private browsing turned on",
+                "copy": "Sorry, you'll need to turn off Private Browsing in your browser's settings to access DK Findout!"
+            }, {
+                "name": "introExpand",
+                "description": "This is the expand call to action for an articles intro copy",
+                "copy": "Read more &rsaquo;",
+            }, {
+                "name": "relatedExpand",
+                "description": "This is the expand call to action for related pages bullets",
+                "copy": "Find out more &rsaquo;",
+            }, {
+                "name": "relatedClose",
+                "description": "This is the close call to action for related pages bullets",
+                "copy": "Find out more ^"
+            }, {
+                "name": "introClose",
+                "description": "This is the close call to action for an articles intro copy",
+                "copy": "Close ^"
+            }, {
+                "name": "searchPlaceholder",
+                "description": "Search field placeholder text",
+                "copy": "I want to find out about..."
+            }, {
+                "name": "videoError",
+                "description": "Error message if cant connect to YouTube",
+                "copy": "Sorry, can't connect to Youtube.<br/><br/>Its possible your school is blocking access to it, or maybe you've lost your internet connection.<br/><br/>You might be able to see this video if you try again from home."
+            }, {
+                "name": "searchError",
+                "description": "Error message shown if attempt to search without keyword",
+                "copy": "Please enter a keyword to search for"
+            }, {
+                "name": "searchErrorSubject",
+                "description": "Error message shown if attempt to search without choosing subject",
+                "copy": "Please choose a subject to search in"
+            }, {
+                "name": "videoSupport",
+                "description": "Error message if browser not supported by YouTube",
+                "copy": "Sorry, this browser isnt supported by Youtube. You'll need to upgrade your browser to see this video."
+            }, {
+                "name": "audioSupport",
+                "description": "Error message if browser doesnt support HTML5 audio",
+                "copy": "Sorry, this browser can't play sound, please upgrade"
+            }, {
+                "name": "cookiesMessage",
+                "description": "Cookie messaging on first site visit",
+                "copy": "This site uses cookies and by continuing to browse it you are agreeing to our use of cookies."
+            }, {
+                "name": "cookiesLink",
+                "description": "Cookie link on first site visit",
+                "copy": "Read our cookies policy"
+            }, {
+                "name": "languageHeader",
+                "description": "Header on select language view",
+                "copy": "Select language"
+            }, {
+                "name": "languageMessage",
+                "description": "Message on select language view",
+                "copy": "You can change your selected language at anytime from the site footer"
+            }, {
+                "name": "welcomeHeader",
+                "description": "Welcome message on select language view",
+                "copy": "Welcome to <span class='logo'>DK Find Out!</span>"
+            }, {
+                "name": "alertFormHeader",
+                "description": "Header to alert form on homepage",
+                "copy": "<span class='hide'>DKFindOut!</span><img src='/images/ie/logo.png' alt='' role='presentation' />Education Newsletter"
+            }, {
+                "name": "alertFormDesc",
+                "description": "Intro text to alert form on homepage",
+                "copy": "Sign up now for the free DKfindout! education email and be the first to hear about new content, DK&rsquo;s latest books, and exciting competitions and give aways."
+            }, {
+                "name": "savingMessage",
+                "description": "Text to display when saving",
+                "copy": "Saving..."
+            }, {
+                "name": "saveMessage",
+                "description": "Text to display when saving",
+                "copy": "SAVE"
+            }, {
+                "name": "copyingMessage",
+                "description": "Text to display when copying",
+                "copy": "Copying..."
+            }, {
+                "name": "deletingMessage",
+                "description": "Text to display when deleting",
+                "copy": "Deleting..."
+            }, {
+                "name": "editMessage",
+                "description": "Text to display when saving",
+                "copy": "EDIT"
+            }
+        ],
+        menuLinks: [
+            {
+                "name": "Kids Club",
+                "url": "kids-club",
+            }, {
+                "name": "Parents",
+                "url": "parents",
+            }, {
+                "name": "Teachers",
+                "url": "teachers",
+            }, {
+                "name": "News",
+                "url": "news",
+            }
+        ]
+    },
+    {
+        "id": "en",
+        "code": "en-us",
+        "directory": "us",
+        "title": "US English",
+        "aboutDK": {
+            "url": "http://www.dk.com/information/about-dk/",
+            "title": "About DK"
+        },
+        "DKImages": {
+            "url": "https://www.dkimages.com/",
+            "title": "DK Images"
+        },
+        dialog: [{
+            "name": "delete-lesson-plan",
+            "title": "Are you sure you want to delete this lesson plan?",
+            "description": "",
+            "buttons": [{"title": "No, I want to keep it &rsaquo;", "type": 1, "url": "javascript:;", "destination": true}, {"title":"Yes, delete forever","type": 2,"url": "javascript:;", "destination": true}]
+        }],
+        meta: [
+            {
+                "name": "not-found",
+                "title": "Page not found",
+                "description": ""
+            }, {
+                "name": "home",
+                "title": "DK Find Out! | Fun Facts for Kids on Animals, Earth, History and more!",
+                "description": "Visit DK Find Out! to learn about dinosaurs, space, animals, nature &amp; more. Fun learning activities for kids with videos, quizzes & galleries at DKFindOut.com"
+            }, {
+               "name": "discover",
+               "title": "Discover | DK Find Out!",
+               "description": "Discover all the subjects on DK Find Out"
+            }, {
+                "name": "explore",
+                "title": "Explore | DK Find Out!",
+                "description": ""
+            }
+        ],
+        staticMessaging: [
+            {
+                "name": "homeIntro",
+                "description": "This is the intro copy on the homepage",
+                "copy": "What do you want to find out?",
+            }, {
+                "name": "homeSubjects",
+                "description": "Header for subjects on HP",
+                "copy": "What&#8217;s in <span class='hide'>DKFindOut!</span><img src='/images/ie/logo.png' alt='' role='presentation' />",
+            }, {
+                "name": "homeStatics",
+                "description": "Header for parents/teachers on HP",
+                "copy": "There&#8217;s more to <span class='hide'>DKFindOut!</span><img src='/images/ie/logo.png' alt='' role='presentation' />",
+            }, {
+                "name": "copyright",
+                "description": "This is the footer legal information",
+                "copy": "<p>&copy; 2015 Dorling Kindersley Limited. Registered Number 01177822 England. Registered Office: 80 Strand, London, WC2R 0RL.</p><p>'Dorling Kindersley', 'DK', 'Eyewitness' and the open book logo <img src='http://res.cloudinary.com/dorling-kindersley/image/upload/DK/9b5cd3b1a3ee4d48b3b1cbf37f5d51d3/0edf65b931034cde91ccca3f89610565.jpg' alt='DK Books' style='vertical-align: middle;'> are trade marks of Dorling Kindersley Limited.</p>",
+            }, {
+                "name": "privateBrowsing",
+                "description": "Message if private browsing turned on",
+                "copy": "Sorry, you'll need to turn off Private Browsing in your browser's settings to access DK Findout!"
+            }, {
+                "name": "introExpand",
+                "description": "This is the expand call to action for an articles intro copy",
+                "copy": "Read more &rsaquo;",
+            }, {
+                "name": "relatedExpand",
+                "description": "This is the expand call to action for related pages bullets",
+                "copy": "Find out more &rsaquo;",
+            }, {
+                "name": "relatedClose",
+                "description": "This is the close call to action for related pages bullets",
+                "copy": "Find out more ^"
+            }, {
+                "name": "introClose",
+                "description": "This is the close call to action for an articles intro copy",
+                "copy": "Close ^"
+            }, {
+                "name": "searchPlaceholder",
+                "description": "Search field placeholder text",
+                "copy": "I want to find out about..."
+            }, {
+                "name": "videoError",
+                "description": "Error message if cant connect to YouTube",
+                "copy": "Sorry, can't connect to Youtube.<br/><br/>Its possible your school is blocking access to it, or maybe you've lost your internet connection.<br/><br/>You might be able to see this video if you try again from home."
+            }, {
+                "name": "searchError",
+                "description": "Error message shown if attempt to search without keyword",
+                "copy": "Please enter a keyword to search for"
+            }, {
+                "name": "searchErrorSubject",
+                "description": "Error message shown if attempt to search without choosing subject",
+                "copy": "Please choose a subject to search in"
+            }, {
+                "name": "videoSupport",
+                "description": "Error message if browser not supported by YouTube",
+                "copy": "Sorry, this browser isnt supported by Youtube. You'll need to upgrade your browser to see this video."
+            }, {
+                "name": "audioSupport",
+                "description": "Error message if browser doesnt support HTML5 audio",
+                "copy": "Sorry, this browser can't play sound, please upgrade"
+            }, {
+                "name": "cookiesMessage",
+                "description": "Cookie messaging on first site visit",
+                "copy": "This site uses cookies and by continuing to browse it you are agreeing to our use of cookies."
+            }, {
+                "name": "cookiesLink",
+                "description": "Cookie link on first site visit",
+                "copy": "Read our cookies policy"
+            }, {
+                "name": "languageHeader",
+                "description": "Header on select language view",
+                "copy": "Select language"
+            }, {
+                "name": "languageMessage",
+                "description": "Message on select language view",
+                "copy": "You can change your selected language at anytime from the site footer"
+            }, {
+                "name": "welcomeHeader",
+                "description": "Welcome message on select language view",
+                "copy": "Welcome to <span class='logo'>DK Find Out!</span>"
+            }, {
+                "name": "alertFormHeader",
+                "description": "Header to alert form on homepage",
+                "copy": "<span class='hide'>DKFindOut!</span><img src='/images/ie/logo.png' alt='' role='presentation' />Education Newsletter"
+            }, {
+                "name": "alertFormDesc",
+                "description": "Intro text to alert form on homepage",
+                "copy": "Sign up now for the free DKfindout! education email and be the first to hear about new content, DK&rsquo;s latest books, and exciting competitions and give aways."
+            }, {
+                "name": "savingMessage",
+                "description": "Text to display when saving",
+                "copy": "Saving..."
+            }, {
+                "name": "saveMessage",
+                "description": "Text to display when saving",
+                "copy": "SAVE"
+            }, {
+                "name": "copyingMessage",
+                "description": "Text to display when copying",
+                "copy": "Copying..."
+            }, {
+                "name": "deletingMessage",
+                "description": "Text to display when deleting",
+                "copy": "Deleting..."
+            }, {
+                "name": "editMessage",
+                "description": "Text to display when saving",
+                "copy": "EDIT"
+            }
+        ],
+        menuLinks: [
+            {
+                "name": "Kids Club",
+                "url": "kids-club",
+            }, {
+                "name": "Parents",
+                "url": "parents",
+            }, {
+                "name": "Teachers",
+                "url": "teachers",
+            }, {
+                "name": "News",
+                "url": "news",
+            }
+        ]
+    }    
+]
---
+++ js/environment.js
@@ -0,0 +1,21 @@
+// ENVIRONMENT
+
+// live
+var previewApplication = false;
+var devApplication = false;
+var stagingApplication = false;
+// preview
+//previewApplication = true;
+// dev
+//devApplication = true;
+// staging
+//stagingApplication = true;
+
+// TODO add domain?
+
+// Declare YT variable to prevent error before YT API script loaded
+var YT;
+
+// We are using language directory in URL so set langDir to true 
+//var langDir = false;
+var langDir = true;
